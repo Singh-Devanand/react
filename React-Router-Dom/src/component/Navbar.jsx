99519c6867440e623aabdff7bd7a8b9f70700248
Devanand Singh <devanandsingh61157@gmail.com>
2026-02-01 02:26:26 +0530

    react router dom done

---
+++ React-Router-Dom/src/component/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='nav'>
+        <h3>
+          Sheryians
+        </h3>
+        <div>
+          <Link to='/'>Home</Link>
+           <Link to='/about'>About</Link>
+           <Link to='/product'>Product</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
